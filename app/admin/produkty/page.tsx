be40ea5528ee6a13071ecc77be21b59d3a6f60ea
beranekjiri86-ux <beranekjiri.86@gmail.com>
2026-03-04 10:34:01 +0100

    Create page.tsx

---
+++ app/admin/produkty/page.tsx
@@ -0,0 +1,10 @@
+import AdminProductsClient from "./ui";
+
+export default function AdminProductsPage() {
+  return (
+    <main className="container">
+      <h1>Produkty</h1>
+      <AdminProductsClient />
+    </main>
+  );
+}
