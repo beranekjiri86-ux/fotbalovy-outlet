c621e0b81d61992a6fd35c4d963cd1527d5c28b4
beranekjiri86-ux <beranekjiri.86@gmail.com>
2026-03-05 14:28:47 +0100

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createSupabaseBrowserClient } from "@/lib/supabase";
 
-export const supabase = createClient(
+export const supabase = createSupabaseBrowserClient();
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
