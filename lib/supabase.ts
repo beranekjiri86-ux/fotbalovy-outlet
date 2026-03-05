e71db55811ae44917da4a113f11180a9fe62044b
beranekjiri86-ux <beranekjiri.86@gmail.com>
2026-03-05 13:28:02 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -18,7 +18,7 @@ export function createSupabaseServerClient() {
       getAll() {
         return cookieStore.getAll();
       },
- setAll(cookiesToSet: Array<{ name: string; value: string; options?: Partial<ResponseCookie> }>) {
+setAll(cookiesToSet: Array<{ name: string; value: string; options?: any }>) {
   cookiesToSet.forEach(({ name, value, options }) => {
     cookieStore.set(name, value, options);
   });
